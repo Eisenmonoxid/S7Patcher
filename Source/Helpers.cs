f02b30cbe0a8fc6d1fb72d408e3468fc970aacd3
Eisenmonoxid <eisenmonoxid@gmail.com>
2025-08-07 02:00:36 +0200

    Small update

--- Source/Helpers.cs
+++ Source/Helpers.cs
@@ -6,8 +6,6 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using Windows.Devices.Power;
 
 namespace S7Patcher.Source
 {
